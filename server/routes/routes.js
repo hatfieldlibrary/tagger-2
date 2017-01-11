4f4331e41824d29c67ab6c75677bbffd02cca59d
mspalti <mspalti@gmail.com>
2017-01-10 17:15:40 -0800

    chore: Refactored file names for public API controllers.

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -21,12 +21,12 @@ module.exports = function(app,config,passport){
 
   const userInfo = require('../../server/api/tagger/controllers/user-info');
   const tag = require('../../server/api/tagger/controllers/tags/admin');
-  const apiTag = require('../../server/api/tagger/controllers/tags/client');
+  const apiTag = require('../api/tagger/controllers/tags/public');
   const tagTarget = require('../../server/api/tagger/controllers/tag-target.js');
   const area = require('../../server/api/tagger/controllers/area');
   const content = require('../../server/api/tagger/controllers/content');
   const collection = require('../api/tagger/controllers/collection/admin');
-  const apiCollection = require('../api/tagger/controllers/collection/client');
+  const apiCollection = require('../api/tagger/controllers/collection/public');
   const category = require('../../server/api/tagger/controllers/category');
   const users = require('../../server/api/tagger/controllers/users');
   /**
