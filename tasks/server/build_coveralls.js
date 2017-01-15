c66f7b9e0070c3534bbce4339e77543c1ed99fdf
mspalti <mspalti@gmail.com>
2017-01-15 10:29:06 -0800

    Work on coveralls configuration

--- tasks/server/build_coveralls.js
+++ tasks/server/build_coveralls.js
@@ -8,6 +8,7 @@ import coveralls from 'gulp-coveralls';
 import gulp from 'gulp';
 
 gulp.task('coveralls', function() {
-  return gulp.src(__dirname + '/coverage/lcov.info')
-    .pipe(coveralls());
+  return gulp.src('./coverage/lcov.info')
+    .pipe(coveralls())
+    .on('error', function(err) {console.log(err)} );
 });
