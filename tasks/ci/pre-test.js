eb706b1aa9b184bbbc050c6c8820a0f5e696661d
Michael W Spalti <mspalti@wu-164283.willamette.edu>
2017-01-18 16:55:09 -0800

    More work on unit tests

--- tasks/ci/pre-test.js
+++ tasks/ci/pre-test.js
@@ -6,11 +6,11 @@ const isparta = require('isparta');
 import istanbul from 'gulp-istanbul';
 import {path, tasks} from './const';
 
-const SERVER_JS = path.SERVER + "/api/**/*.js";
-const CLIENT_JS = [path.CLIENT + "/dev/app/**/*.js", '!' + path.CLIENT + '/dev/bower_components/**/*.js'];
+const SERVER_JS = path.SERVER + '/api/**/*.js';
+const CLIENT_JS = path.CLIENT + '/dev/app/**/*.js, !' + path.CLIENT + '/dev/bower_components/**/*.js';
 
 gulp.task(tasks.ISTANBUL_PREFIGHT, () => {
-  return gulp.src(CLIENT_JS, SERVER_JS)
+  return gulp.src([CLIENT_JS, SERVER_JS])
     .pipe(istanbul({includeUntested: true, instrumenter: isparta.Instrumenter}))
     // This overwrites `require` so it returns covered files
     .pipe(istanbul.hookRequire());
